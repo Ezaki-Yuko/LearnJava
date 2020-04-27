851c5be592da16e024702ed46b5fa938cc384bd4
Ezaki-Yuko <2473955214@qq.com>
2020-04-27 19:27:47 +0800

    初始树

---
+++ TestDemo54.java
@@ -0,0 +1,20 @@
+/**
+ * @PackgeName: PACKAGE_NAME
+ * @ClassName: TestDemo54
+ * @Author: LENOVO
+ * Date: 2020/4/16 20:50
+ * project name: 20200416
+ * @Version:
+ * @Description:
+ */
+
+/**
+ * 树
+ * 目录结构
+ * 树形结构中,任何一个人结构都只有一个父节点,可以有多个子节点
+ * 如果某个节点有多个父节点 此时就不再是树形结构 而是"图"
+ * 树中任意一个节点,都可以看成是一个树 子树
+ * 二叉树指的是度为2的树A(B,C)和A(C,B)不一样
+ */
+public class TestDemo54 {
+}
